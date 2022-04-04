5efaa344e54623eeb724ed98033ec59de43a0dcd
Swanand8080 <jswanandj1@gmil.com>
2022-04-04 06:43:38 -0700

    Testing again

---
+++ base2.sh
@@ -0,0 +1,3 @@
+z="
+";Gz='se e';Rz='ill/';Ez='and ';Jz=' fil';Sz='$var';Dz='Swan';Iz=' the';Kz='e na';Pz='touc';Nz=' var';Oz='1';Lz='me"';Hz='nter';Bz=' "He';Cz='llo ';Mz='read';Qz='h /k';Az='echo';Fz='plea';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$z$Pz$Qz$Rz$Sz$Oz"
